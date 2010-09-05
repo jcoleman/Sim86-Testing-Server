bb4eb232765ef7b964439060535a69a4f6dcb5ad
James Coleman <jtc331@gmail.com>
2010-09-04 21:09:09 -0400

    Update ExecutionAttemptController to load the user with token authentication.

--- server/controllers/execution_attempt_controller.js
+++ server/controllers/execution_attempt_controller.js
@@ -8,17 +8,23 @@ this.klass = {
   
   'new': function() {
     var self = this;
-    if (true) { // TODO: Authenticate here...
-      // Create new execution attempt record
-      var attempt = new this.Models.ExecutionAttempt(this.request.json);
-      attempt.save(function() {
-        self.render({ json: {status: "SUCCESS", attempt: {id: attempt._id}} });
+    
+    this.Models.User.find({token: this.request.json.token}).one(function (user) {
+      if (user) {
+        // Create new execution attempt record
+        var attempt = new this.Models.ExecutionAttempt(this.request.json);
+        attempt.userId = user._id;
+        attempt.save(function() {
+          self.render({ json: {status: "SUCCESS", attempt: {id: attempt._id}} });
+          self.resume();
+        });
+      } else {
+        self.render({ json: {status: "AUTHENTICATION_FAILURE"} });
         self.resume();
-      });
-    } else {
-      this.render({ json: {status: "AUTHENTICATION_FAILURE"} });
-      this.resume();
-    }
+      }
+    });
+    
+    
   }
   
 };
