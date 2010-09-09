747d2353cd718e1d3773962d8b9114af37afbcd2
James Coleman <jtc331@gmail.com>
2010-09-08 20:49:51 -0400

    Use the id() helper; publish websocket event on record save success.

--- server/controllers/execution_record_controller.js
+++ server/controllers/execution_record_controller.js
@@ -14,10 +14,16 @@ this.klass = {
         if (attempt) {
           // Create new execution record
           var record = new self.Models.ExecutionRecord(self.request.json.object);
-          record.attemptId = attempt._id;
+          record.attemptId = attempt.id();
           record.save(function() {
             self.render({ json: {status: "SUCCESS", record: {id: record._id}} });
             self.resume();
+            try {
+              self.WebSocket.publishEvent('executionAttempt', record.attemptId, {object: record});
+            } catch (e) {
+              require('sys').log("Exception occurred try to publish execution attempt record event: " + e);
+            }
+            
           });
         } else {
           self.render({ json: {status: "INVALID_ATTEMPT_ID"} });
