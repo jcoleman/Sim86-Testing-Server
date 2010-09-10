d9db4e55c84870cefe7be793681796d2caf0b06b
James Coleman <jtc331@gmail.com>
2010-09-09 23:01:21 -0400

    Large refactor/feature adding to messaging in client.

--- client/message.js
+++ client/message.js
@@ -9,38 +9,130 @@ Sim.Messenger = {
     io.setPath('/client/');
     Sim.Messenger.socket = new io.Socket('localhost', {port:3000});
     Sim.Messenger.socket.connect();
-    Sim.Messenger.socket.on('message', function(data) {
-      var message = data.evalJSON();
-      
-      var replyId = message.replyId;
-      if (replyId) {
-        var callback = Sim.Messenger.callbacks[replyId];
-        if (callback) {
-          callback(message);
-        }
-      }
-    });
+    Sim.Messenger.socket.on('message', Sim.Messenger._processRemoteMessage);
   },
   
-  send: function(action, object, callback) {
-    var replyId = ++Sim.Messenger.currentMessageId;
+  sendLocal: function(action, object, callback, responseId) {
+    var message = new Sim.Message('local', action, object, null, responseId);
     
     if (callback) {
-      Sim.Messenger.callbacks[replyId] = callback;
+      Sim.Messenger.callbacks[message.replyId] = callback;
     }
-    console.log('sending message', action, replyId, object);
-    Sim.Messenger.socket.send(Object.toJSON({
-      replyId: replyId,
-      action: action,
-      object: object
-    }));
-  },
-  
-  receive: function() {
     
+    console.log('Sending message', action, message.replyId, object);
+    Sim.Messenger._emitToReceivers(message);
+    
+    return message;
   },
   
-  currentMessageId: 1000,
+  sendRemote: function(action, object, callback, responseId) {
+    var message = new Sim.Message('remote', action, object, null, responseId);
+    
+    if (callback) {
+      Sim.Messenger.callbacks[message.replyId] = callback;
+    }
+    
+    console.log('Sending message', action, message.replyId, object);
+    Sim.Messenger.socket.send( Object.toJSON(message) );
+    
+    return message;
+  },
+  
+  receive: function(discriminator, action, callback) {
+    var receivers = Sim.Messenger.receivers;
+    
+    var actionReceivers = receivers[action];
+    if (!actionReceivers) {
+      actionReceivers = receivers[action] = [];
+    }
+    
+    var receiver = actionReceivers.find(function (_receiver) {
+      return _receiver.discriminator == discriminator;
+    });
+    
+    if (!receiver) {
+      receiver = { discriminator: discriminator, callbacks: [] };
+      actionReceivers.push(receiver);
+    }
+    
+    receiver.callbacks.push(callback);
+  },
+  
+  ignore: function(discriminator, action) {
+    if (Sim.Messenger.receivers[action]) {
+      Sim.Messenger.receivers[action] = Sim.Messenger.receivers[action].without(function (receiver) {
+        return receiver.discriminator == discriminator;
+      });
+    }
+  },
+  
+  _processRemoteMessage: function(data) {
+    var parsed = data.evalJSON();
+    
+    var message = new Sim.Message('remote', parsed.action, parsed.object, parsed.replyId, parsed.responseId);
+    message.success = parsed.success;
+    
+    console.log("Processing remote message", parsed, message);
+    Sim.Messenger._fireCallbackFor(message);
+    Sim.Messenger._emitToReceivers(message);
+  },
+  
+  _emitToReceivers: function(message) {
+    if (message.action) {
+      var receivers = Sim.Messenger.receivers[message.action];
+      console.log("receivers:", receivers);
+      if (receivers) {
+        receivers.each(function (receiver) {
+          receiver.callbacks.each(function (callback) {
+            if (callback) { callback(message); }
+          });
+        });
+      }
+    }
+  },
+  
+  _fireCallbackFor: function(message) {
+    var responseId = message.responseId;
+    if (responseId) {
+      var callback = Sim.Messenger.callbacks[responseId];
+      if (callback) {
+        callback(message);
+      }
+      delete Sim.Messenger.callbacks[responseId];
+    }
+  },
+  
+  _currentMessageId: 1000,
+  
+  receivers: {},
+  
   callbacks: {}
   
 };
+
+Sim.Message = Class.create({
+  
+  initialize: function(type, action, object, replyId, responseId) {
+    this.type = type;
+    this.action = action;
+    this.object = object;
+    this.responseId = responseId;
+    this.replyId = replyId || this._nextMessageId();
+  },
+  
+  reply: function(object, callback) {
+    if (this.type == 'remote') {
+      Sim.Messenger.sendRemote(null, object, callback, this.replyId);
+    } else if (this.type == 'local') {
+      Sim.Messenger.sendLocal(null, object, callback, this.replyId);
+    }
+  },
+  
+  _currentMessageId: 1000,
+  
+  _nextMessageId: function() {
+    console.log('creating new message id', Sim.Messenger._currentMessageId + 1);
+    return ++Sim.Messenger._currentMessageId;
+  }
+  
+});
