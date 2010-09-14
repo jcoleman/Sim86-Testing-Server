dc1ed69a08637d6d56ad1ec3f4232c8fab37a7cc
James Coleman <jtc331@gmail.com>
2010-09-14 17:19:21 -0400

    Add initial styling to make the page semi-usable.

--- client/ui/controllers/application.js
+++ client/ui/controllers/application.js
@@ -17,16 +17,16 @@ Sim.UI.ApplicationController = Class.create(Sim.UI.Controller, {
       }
       
       self.attemptShowController = new Sim.UI.Attempt.ShowController(
-        self.element,
+        self.grab('.attempt-display'),
         {attempt: message.object}
       );
     });
     
-    this.loginController = new Sim.UI.LoginController(this.element, {onLogin: function(user) {
+    this.loginController = new Sim.UI.LoginController(this.grab('.login'), {onLogin: function(user) {
       this.currentUser = user;
       self.loginController.destroy();
       self.loginController = null;
-      self.attemptsList = new Sim.UI.Attempt.ListController(self.element, {});
+      self.attemptsList = new Sim.UI.Attempt.ListController(self.grab('.attempt-selector'), {});
     }});
   },
   
