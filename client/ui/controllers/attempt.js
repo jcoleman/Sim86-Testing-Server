39b01f1485065f6faa44eb0c54a3696f5d546e27
James Coleman <jtc331@gmail.com>
2010-09-18 19:12:38 -0400

    Display the attempt details (such as error/correct counts) along with the execution display.

--- client/ui/controllers/attempt.js
+++ client/ui/controllers/attempt.js
@@ -14,6 +14,8 @@ Sim.UI.Attempt.ShowController = Class.create(Sim.UI.Controller, {
     $super();
     var self = this;
     
+    this.cacheDisplayElements();
+    
     this.currentInstructionCount = 0;
     this.instructionCache = {};
     
@@ -22,7 +24,7 @@ Sim.UI.Attempt.ShowController = Class.create(Sim.UI.Controller, {
     Sim.Messenger.receive(this, 'feed.executionRecord', function (message) {
       var object = message.object;
       if (self.attempt._id == object.record.attemptId) {
-        self.updateInstructionDisplay(object);
+        self.updateDisplays(object);
       }
     });
     
@@ -46,19 +48,19 @@ Sim.UI.Attempt.ShowController = Class.create(Sim.UI.Controller, {
   },
   
   initializeExecutionDisplay: function() {
-    this.executionDisplayController = new Sim.UI.Execution.DisplayController(this.element);
+    this.executionDisplayController = new Sim.UI.Execution.DisplayController(this.grab('.execution-display-container'));
   },
   
   displayInstructionAtCount: function(count) {
     var self = this;
     if (this.instructionCache[count]) {
-      this.updateInstructionDisplay(this.instructionCache[count]);
+      this.updateDisplays(this.instructionCache[count]);
     } else {
       Sim.Messenger.sendRemote('retrieve.executionRecord', {count: count, attempt: this.attempt}, function (message) {
         var object = message.object;
         if (object.record) {
           self.instructionCache[count] = object;
-          self.updateInstructionDisplay(object);
+          self.updateDisplays(object);
         } else {
           alert('No record found for this attempt with instruction count: ' + count);
         }
@@ -66,9 +68,23 @@ Sim.UI.Attempt.ShowController = Class.create(Sim.UI.Controller, {
     }
   },
   
-  updateInstructionDisplay: function(object) {
+  updateDisplays: function(object) {
     this.currentInstructionCount = object.record.count;
+    
+    if (object.attempt) { this.updateAttemptDetails(object.attempt); }
     this.executionDisplayController.update(object.record, object.reference);
+  },
+  
+  updateAttemptDetails: function(attempt) {
+    for (var i = 0, len = this.attemptCountsKeys.length; i < len; ++i) {
+      var key = this.attemptCountsKeys[i];
+      this.elementCache.counts[key].update(attempt[key]);
+    }
+    
+    for (var i = 0, len = this.attemptErrorKeys.length; i < len; ++i) {
+      var key = this.attemptErrorKeys[i];
+      this.elementCache.errors[key].update(attempt.errorsByType[key]);
+    }
   },
   
   beginLiveStream: function() {
@@ -81,6 +97,32 @@ Sim.UI.Attempt.ShowController = Class.create(Sim.UI.Controller, {
     this.liveStream = false;
     Sim.Messenger.sendRemote('unsubscribe.executionAttempt', {attemptId: this.attempt._id});
     this.liveStreamControl.update('Enable live updates');
+  },
+  
+  elementCache: {},
+  
+  attemptErrorKeys: [
+    'registers', 'flags', 'memoryChangeAddresses', 'memoryChangeValues',
+    'operandTypes', 'operandStrings', 'instructionAddressingMode',
+    'instructionSegment', 'instructionOffset', 'instructionMnemonic', 'rawBytes'
+  ],
+  
+  attemptCountsKeys: [
+    'recordCount', 'correctCount', 'incorrectCount'
+  ],
+  
+  cacheDisplayElements: function() {
+    var self = this;
+    
+    this.elementCache.errors = {};
+    this.attemptErrorKeys.each(function (key) {
+      self.elementCache.errors[key] = self.grab('.attempt-error-' + key);
+    });
+    
+    this.elementCache.counts = {};
+    this.attemptCountsKeys.each(function (key) {
+      self.elementCache.counts[key] = self.grab('.attempt-count-' + key);
+    });
   },
   
   destroy: function($super) {
