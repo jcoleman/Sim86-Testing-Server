a987d0bc8f7c4bc8654dd6387f75f863e43bd8b9
James Coleman <jtc331@gmail.com>
2010-09-20 21:25:20 -0400

    Large refactor of the execution record controller: break out parts into individual methods moving the record save to the end so that we can eventually modify the record after the reference comparison has occurred.

--- server/controllers/execution_record_controller.js
+++ server/controllers/execution_record_controller.js
@@ -9,56 +9,11 @@ this.klass = {
   'new': function() {
     var self = this;
     console.log('complete body: ' + this.request.completeBody);
+    
     if (this.request.json.attemptId) {
       this.Models.ExecutionAttempt.find({_id: this.request.json.attemptId}).one(function(attempt) {
         if (attempt) {
-          // Create new execution record
-          var record = new self.Models.ExecutionRecord(self.request.json.object);
-          record.attemptId = attempt.id();
-          record.normalize();
-          record.save(function() {
-            self.render({ json: {status: "SUCCESS", record: {id: record._id}} });
-            self.resume();
-            
-            self.Models.User.getSystemAttemptForModule(attempt.executionModuleId, function (systemAttempt) {
-              var publish = function(_referenceRecord, _attempt) {
-                try {
-                  self.WebSocket.publishEvent( 'executionRecord',
-                                               record.attemptId,
-                                               { record: record.__doc,
-                                                 reference: _referenceRecord,
-                                                 attempt: _attempt.__doc } );
-                } catch (e) {
-                  require('sys').log("Exception occurred try to publish execution attempt record event: " + e);
-                }
-              };
-              
-              if (systemAttempt) {
-                self.Models.ExecutionRecord.find({
-                  attemptId: systemAttempt.id(),
-                  count: record.count
-                }, false).one(function (reference) {
-                  ++attempt.recordCount;
-                  
-                  var errorDescriptor = record.compareToReference(reference || self.ExecutionModel.getBlankDocument());
-                  if (errorDescriptor[0]) {
-                    // Incorrect... update attempt with error counts/types
-                    ++attempt.incorrectCount;
-                    self._updateAttemptWithErrors(attempt, errorDescriptor[1]);
-                  } else {
-                    // Correct
-                    ++attempt.correctCount;
-                  }
-                  
-                  attempt.save(function() {
-                    publish(reference, attempt);
-                  });
-                });
-              } else {
-                publish(null);
-              }
-            });
-          });
+          self._savePostedRecordForAttempt(attempt);
         } else {
           self.render({ json: {status: "INVALID_ATTEMPT_ID"} });
           self.resume();
@@ -70,13 +25,70 @@ this.klass = {
     }
   },
   
-  _updateAttemptWithErrors: function(attempt, errors) {
-    var keys = ['registers', 'flags', 'memoryChangeAddresses', 'memoryChangeValues',
-                'operandTypes', 'operandStrings', 'instructionAddressingMode',
-                'instructionSegment', 'instructionOffset', 'instructionMnemonic', 'rawBytes'];
-    for (var i = 0, len = keys.length; i < len; ++i) {
-      var key = keys[i];
-      attempt.errorsByType[key] += errors[key];
+  _savePostedRecordForAttempt: function(attempt) {
+    var self = this;
+    
+    // Create new execution record
+    var record = new this.Models.ExecutionRecord(self.request.json.object);
+    record.attemptId = attempt.id();
+    record.normalize();
+    
+    this._findReferenceAndUpdateAttempt(record, attempt, function (referenceRecord) {
+      record.save(function () {
+        self.render({ json: {status: "SUCCESS", record: {id: record._id}} });
+        self.resume();
+        
+        try {
+          self.WebSocket.publishEvent( 'executionRecord',
+                                       record.attemptId,
+                                       { record: record.__doc,
+                                         reference: referenceRecord,
+                                         attempt: attempt.__doc } );
+        } catch (e) {
+          require('sys').log("Exception occurred try to publish execution attempt record event: " + e);
+        }
+      });
+    });
+  },
+  
+  _findReferenceAndUpdateAttempt: function(record, attempt, callback) {
+    var self = this;
+    
+    this.Models.User.getSystemAttemptForModule(attempt.executionModuleId, function (systemAttempt) {
+      if (systemAttempt) {
+        var finder = { attemptId: systemAttempt.id(), count: record.count };
+        self.Models.ExecutionRecord.find(finder, false).one(function (reference) {
+          ++attempt.recordCount;
+          
+          var errorDescriptor = record.compareToReference(reference || self.ExecutionModel.getBlankDocument());
+          self._updateAttemptWithReferenceDiff(attempt, errorDescriptor);
+          
+          attempt.save(function() {
+            callback(reference);
+          });
+        });
+      } else {
+        callback(null);
+      }
+    });
+  },
+  
+  _updateAttemptWithReferenceDiff: function(attempt, errorDescriptor) {
+    if (errorDescriptor[0]) {
+      // Incorrect... update attempt with error counts/types
+      ++attempt.incorrectCount;
+      
+      var errors = errorDescriptor[1];
+      var keys = ['registers', 'flags', 'memoryChangeAddresses', 'memoryChangeValues',
+                  'operandTypes', 'operandStrings', 'instructionAddressingMode',
+                  'instructionSegment', 'instructionOffset', 'instructionMnemonic', 'rawBytes'];
+      for (var i = 0, len = keys.length; i < len; ++i) {
+        var key = keys[i];
+        attempt.errorsByType[key] += errors[key];
+      }
+    } else {
+      // Correct
+      ++attempt.correctCount;
     }
   }
   
