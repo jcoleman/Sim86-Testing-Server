3523268a23c77c23ba708a107ac8ab8e248d6c2f
James Coleman <jtc331@gmail.com>
2010-10-23 10:27:28 -0400

    Verify the object format of an execution record submission in order to fail early if there is an issue.

--- server/controllers/execution_record_controller.js
+++ server/controllers/execution_record_controller.js
@@ -9,18 +9,67 @@ this.klass = {
   'new': function() {
     var self = this;
     
-    if (this.request.json.attemptId) {
-      this.Models.ExecutionAttempt.find({_id: this.request.json.attemptId}).one(function(attempt) {
-        if (attempt) {
-          self._savePostedRecordForAttempt(attempt);
-        } else {
-          self.render({ json: {status: "INVALID_ATTEMPT_ID"} });
-          self.resume();
-        }
-      });
+    // Verify data structure...
+    var json = this.request.json;
+    var structureErrors = [];
+    if (!json.object || json.object.toString() !== '[object Object]') {
+      structureErrors.push('`object` was either not found or not a map.');
     } else {
-      this.render({ json: {status: "INVALID_ATTEMPT_ID"} });
+      var object = json.object;
+      if (!object.instruction || object.instruction.toString() !== '[object Object]') {
+        structureErrors.push('`object.instruction` was either not found or not a map.');
+      } else {
+        var instruction = object.instruction;
+        if (instruction.operands === null || instruction.operands === undefined || !Object.isArray(instruction.operands)) {
+          structureErrors.push('`object.instruction.operands` was either not found or not an array.');
+        } else {
+          var operands = instruction.operands;
+          for (var i, len = operands.length; i < len; ++i) {
+            if (!operands[i] || operands[i].toString() !== '[object Object]') {
+              structureErrors.push('`object.instruction.operands[' + i + ']` was either not found or not a map.');
+            }
+          }
+        }
+      }
+      
+      if (!object.registers || object.registers.toString() !== '[object Object]') {
+        structureErrors.push('`object.registers` was either not found or not a map.');
+      }
+      
+      if (!object.memory || object.memory.toString() !== '[object Object]') {
+        structureErrors.push('`object.memory` was either not found or not a map.');
+      } else {
+        var memory = object.memory;
+        if (memory.changes === null || memory.changes === undefined || !Object.isArray(memory.changes)) {
+          structureErrors.push('`object.memory.changes` was either not found or not an array.');
+        } else {
+          var changes = memory.changes;
+          for (var i, len = changes.length; i < len; ++i) {
+            if (!changes[i] || changes[i].toString() !== '[object Object]') {
+              structureErrors.push('`object.memory.changes[' + i + ']` was either not found or not a map.');
+            }
+          }
+        }
+      }
+    }
+    
+    if (structureErrors.length > 0) {
+      this.render({ json: {status: "INVALID_OBJECT_STRUCTURE", errors: structureErrors} });
       this.resume();
+    } else {
+      if (this.request.json.attemptId) {
+        this.Models.ExecutionAttempt.find({_id: this.request.json.attemptId}).one(function(attempt) {
+          if (attempt) {
+            self._savePostedRecordForAttempt(attempt);
+          } else {
+            self.render({ json: {status: "INVALID_ATTEMPT_ID"} });
+            self.resume();
+          }
+        });
+      } else {
+        this.render({ json: {status: "INVALID_ATTEMPT_ID"} });
+        this.resume();
+      }
     }
   },
   
