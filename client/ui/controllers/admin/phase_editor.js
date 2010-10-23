65f8732fd6d31839bf0ff4a2a23fd80ade9ef5df
James Coleman <jtc331@gmail.com>
2010-10-22 21:33:11 -0400

    Finish the phase editor (you can now edit the module list).

--- client/ui/controllers/admin/phase_editor.js
+++ client/ui/controllers/admin/phase_editor.js
@@ -30,6 +30,8 @@ Sim.UI.Admin.PhaseEditorController = Class.create(Sim.UI.Controller, {
       }
     });
     
+    this.setupModuleListEditing();
+    
     this.grab('.save-link').observe('click', function(event) {
       var newObject = !self.phase._id;
       Sim.Messenger.sendRemote('createOrUpdate.phase', self.phase, function(message) {
@@ -40,8 +42,76 @@ Sim.UI.Admin.PhaseEditorController = Class.create(Sim.UI.Controller, {
     });
   },
   
+  setupModuleListEditing: function() {
+    var self = this;
+    if (!self.phase.executionModules) { self.phase.executionModules = []; }
+    
+    this.unusedModuleListController = new Sim.UI.ListController(
+      this.grab('.phase-module-list-container'),
+      {
+        listParent: this.grab('.unused-module-list'),
+        retrieveItems: function(callback) {
+          Sim.Messenger.sendRemote('retrieve.modules', {}, function(message) {
+            var items = (Object.isArray(message.object) ? message.object : []).collect(function (module) {
+              return {
+                executionModuleId: module._id,
+                filename: module.filename
+              };
+            }).findAll(function (item) {
+              return !self.phase.executionModules.find(function(phaseModule) {
+                return phaseModule.executionModuleId == item.executionModuleId;
+              });
+            });
+            callback(items);
+          });
+        },
+        itemTemplate: "admin_unused_module_list_item",
+        onItemRender: function (element, item) {
+          element.select('.module-link').first().observe('click', function(event) {
+            self.phase.executionModules.push(item);
+            self.unusedModuleListController.removeItem(item);
+            self.usedModuleListController.addItem(item);
+            
+            event.stop();
+          });
+        }
+      }
+    );
+    
+    this.usedModuleListController = new Sim.UI.ListController(
+      this.grab('.phase-module-list-container'),
+      {
+        listParent: this.grab('.used-module-list'),
+        retrieveItems: function(callback) {
+          callback(self.phase.executionModules);
+        },
+        itemTemplate: "admin_used_module_list_item",
+        onItemRender: function (element, item) {
+          element.select('.module-link').first().observe('click', function(event) {
+            self.phase.executionModules = self.phase.executionModules.without(item);
+            self.usedModuleListController.removeItem(item);
+            self.unusedModuleListController.addItem(item);
+            
+            event.stop();
+          });
+        }
+      }
+    );
+  },
+  
   getTemplatePath: function() {
     return 'admin_phase_editor';
+  },
+  
+  destroy: function($super) {
+    var self = this;
+    ['unusedModuleListController', 'usedModuleListController'].each(function(prop) {
+      if (self[prop]) {
+        self[prop].destroy();
+        delete self[prop];
+      }
+    });
+    $super();
   }
   
 });
