c7239b24461c65b25dc13d0563a30920fb1a6c0c
James Coleman <jtc331@gmail.com>
2010-10-30 17:26:47 -0400

    Add hex string helper function.

--- client/core.js
+++ client/core.js
@@ -1,3 +1,15 @@
 Sim = {};
 Sim.UI = {};
 Sim.UI.Templates = {};
+
+hex = function(number, zeroExtend) {
+  if (number === null || number === undefined) {
+    return '';
+  }
+  
+  var hs = (number || 0).toString(16);
+  if (!zeroExtend && hs.length < 4) {
+    hs = ('0'.times(4 - hs.length)) + hs;
+  }
+  return '0x' + hs;
+};
